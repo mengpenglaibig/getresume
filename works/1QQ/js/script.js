cca8fd3266827b5914c81293cb42399a53e970e4
mengpenglai <mengpenglai@qq.com>
2016-12-21 15:26:36 +0800

    8

--- works/1QQ/js/script.js
+++ works/1QQ/js/script.js
@@ -6,7 +6,7 @@ if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
 	head = document.getElementsByTagName('head');
 	viewport = document.createElement('meta');
 	viewport.name = 'viewport';
-	viewport.content = 'target-densitydpi=device-dpi, width=' + 640 + 'px, user-scalable=no';
+	viewport.content = 'target-densitydpi=device-dpi, width=' + 750 + 'px, user-scalable=no';
 	head.length > 0 && head[head.length - 1].appendChild(viewport);    
    
 }

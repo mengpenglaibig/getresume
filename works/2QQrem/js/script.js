9c512cdcf5e755d0817677f9d15ffbb6fccd1843
mengpenglai <mengpenglai@qq.com>
2016-12-23 12:33:06 +0800

    35

--- works/2QQrem/js/script.js
+++ works/2QQrem/js/script.js
@@ -1,15 +1,15 @@
 //ios
-if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
-   //alert(navigator.userAgent); 
+// if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+//    //alert(navigator.userAgent); 
    
-   //add a new meta node of viewport in head node
-	head = document.getElementsByTagName('head');
-	viewport = document.createElement('meta');
-	viewport.name = 'viewport';
-	viewport.content = 'target-densitydpi=device-dpi, width=' + 640 + 'px, user-scalable=no';
-	head.length > 0 && head[head.length - 1].appendChild(viewport);    
+//    //add a new meta node of viewport in head node
+// 	head = document.getElementsByTagName('head');
+// 	viewport = document.createElement('meta');
+// 	viewport.name = 'viewport';
+// 	viewport.content = 'target-densitydpi=device-dpi, width=' + 640 + 'px, user-scalable=no';
+// 	head.length > 0 && head[head.length - 1].appendChild(viewport);    
    
-}
+// }
 //android
 //if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
 //  //alert(navigator.userAgent);  
@@ -22,9 +22,15 @@ if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
 //};
 
 $(function(){
-	
-	
 	//页面不足一屏，铺满一屏
-	$('.layout').css('min-height',$(window).height());
+	$('.wrap').css('min-height',$(window).height());
+	$('.wrap').fullpage();
+	$('.sharebtn').on('click',function(){
+		$('.dialog').addClass('show');
+		$('.dialog').on('click',function(){
+			$(this).removeClass('show');
+		})
+	})
 })
+
 

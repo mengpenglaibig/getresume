0652db0213826f1ecdc140046e9e1c0e8fbde253
mengpenglai <mengpenglai@qq.com>
2016-12-23 15:36:40 +0800

    31

---
+++ works/11xiaonuandingkuan/js/script.js
@@ -0,0 +1,46 @@
+//ios
+if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+   //alert(navigator.userAgent); 
+   
+   //add a new meta node of viewport in head node
+	head = document.getElementsByTagName('head');
+	viewport = document.createElement('meta');
+	viewport.name = 'viewport';
+	viewport.content = 'target-densitydpi=device-dpi, width=' + 750 + 'px, user-scalable=no';
+	head.length > 0 && head[head.length - 1].appendChild(viewport);    
+   
+}
+//android
+//if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+//  //alert(navigator.userAgent);  
+//  window.location.href ="iPhone.html";
+//} else if (/(Android)/i.test(navigator.userAgent)) {
+//  //alert(navigator.userAgent); 
+//  window.location.href ="Android.html";
+//} else {
+//  window.location.href ="pc.html";
+//};
+
+$(function(){
+	
+	$('.login-body .header li').on('click',function(){
+		$(this).addClass('selected').siblings().removeClass('selected');
+		$('.login-body .body').hide().eq($(this).index()).show();
+	})
+
+	$('.login-body .body input').on('keyup',function(){
+		var em=$(this).parent().parent().find('em');
+		if($(this).val()!=''){
+			em.show();
+		}else{
+			em.hide();
+		}
+	})
+
+	$('.login-body .body em').click(function(){
+		var ipt=$(this).parent().find('input');
+		ipt.val('');
+	})
+	//页面不足一屏，铺满一屏
+	// $('.layout').css('min-height',$(window).height());
+})

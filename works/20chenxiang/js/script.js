ff07b2108d146c8a0805a44f4dbfd1ec7a260c22
mengpenglai <mengpenglai@qq.com>
2017-03-04 20:20:38 +0800

    2

---
+++ works/20chenxiang/js/script.js
@@ -0,0 +1,36 @@
+//ios
+if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+   //alert(navigator.userAgent); 
+   
+   //add a new meta node of viewport in head node
+	head = document.getElementsByTagName('head');
+	viewport = document.createElement('meta');
+	viewport.name = 'viewport';
+	viewport.content = 'target-densitydpi=device-dpi, width=' + 640 + 'px, user-scalable=no';
+	head.length > 0 && head[head.length - 1].appendChild(viewport);    
+   
+}
+//android
+//if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+//  //alert(navigator.userAgent);  
+//  window.location.href ="iPhone.html";
+//} else if (/(Android)/i.test(navigator.userAgent)) {
+//  //alert(navigator.userAgent); 
+//  window.location.href ="Android.html";
+//} else {
+//  window.location.href ="pc.html";
+//};
+
+$(function(){
+	
+	
+	//页面不足一屏，铺满一屏
+	$('.wrap').css('min-height',$(window).height());
+	$('.header .ico').on('click',function(){
+		if($('.wrap').hasClass('wrap-selected')){
+			$('.wrap').removeClass('wrap-selected')
+		}else{
+			$('.wrap').addClass('wrap-selected');
+		}
+	})
+})

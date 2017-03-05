39371359c1ede82683de30be739a108755f16e27
mengpenglai <mengpenglai@qq.com>
2017-03-05 22:20:25 +0800

    4

---
+++ works/22haitui/js/script.js
@@ -0,0 +1,37 @@
+//ios
+if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+   //alert(navigator.userAgent); 
+   
+   //add a new meta node of viewport in head node
+	head = document.getElementsByTagName('head');
+	viewport = document.createElement('meta');
+	viewport.name = 'viewport';
+	viewport.content = 'target-densitydpi=device-dpi, width=' + 720 + 'px, user-scalable=no';
+	head.length > 0 && head[head.length - 1].appendChild(viewport);    
+   
+}
+//android
+//if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+//  //alert(navigator.userAgent);  
+//  window.location.href ="iPhone.html";
+//} else if (/(Android)/i.test(navigator.userAgent)) {
+//  //alert(navigator.userAgent); 
+//  window.location.href ="Android.html";
+//} else {
+//  window.location.href ="pc.html";
+//};
+
+$(function(){
+	 var wechat_developer_reload = function(){
+		$('body').append('<input type="button" value="refresh" onclick="window.location.reload();"/>');	 
+	} 
+	//wechat_developer_reload();
+	
+	if (window.console) {
+		console.info(">>Join us? Email：developer@qietu.com");
+	}		
+	
+	//页面不足一屏，铺满一屏
+	$('.layout').css('min-height',$(window).height());
+})
+
---
+++ works/23yixinli/js/script.js
@@ -0,0 +1,34 @@
+//ios
+if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+   //alert(navigator.userAgent); 
+   
+   //add a new meta node of viewport in head node
+	head = document.getElementsByTagName('head');
+	viewport = document.createElement('meta');
+	viewport.name = 'viewport';
+	viewport.content = 'target-densitydpi=device-dpi, width=' + 640 + 'px, user-scalable=no';
+	head.length > 0 && head[head.length - 1].appendChild(viewport);    
+   
+}
+//android
+//if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+//  //alert(navigator.userAgent);  
+//  window.location.href ="iPhone.html";
+//} else if (/(Android)/i.test(navigator.userAgent)) {
+//  //alert(navigator.userAgent); 
+//  window.location.href ="Android.html";
+//} else {
+//  window.location.href ="pc.html";
+//};
+
+$(function(){
+	
+	
+	//页面不足一屏，铺满一屏
+	$('.layout').css('min-height',$(window).height());
+	$('.footer li').hover(function(){
+			$(this).addClass('selected');
+		},function(){
+			$(this).removeClass('selected');
+		});	
+})
